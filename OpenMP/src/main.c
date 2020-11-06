16339bf31f3b0897f50af95b74d8363e221588cb
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2020-11-06 18:12:19 -0300

    falha de segmentaçao

--- OpenMP/src/main.c
+++ OpenMP/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <omp.h>
 #include "../auxMatrix/MatrixIO.h"
 #include "multMatrix.h"
@@ -29,16 +30,49 @@ int main (int argc, char *argv[])
     if (argc < 8)
     {
         printf("Uso:\n");
-        printf("%s <numero de linhas> <numero de colunas> <numero de colunas> <arquivo A> <arquivo B> <arquivo C> <arquivo D>", argv[0]);
+        printf("%s <numero de linhas da matriz A> <numero de linhas da matriz B> <numero de linhas da matriz C> <arquivo A> <arquivo B> <arquivo C> <arquivo D>\n", argv[0]);
     }
     else
     {
-        int *matrixA = readMatrix(10, 10, "arqD.dat");
-        printMatrix(matrixA, 10, 10, NULL);
-    }
+        // argv: ./main y w v arqA.dat arqB.dat arqC.dat arqD.dat 
+        int y = atoi(argv[1]);
+        int w = atoi(argv[2]);
+        int v = atoi(argv[3]);
 
-    int *matrixA = readMatrix(10, 10, "arqD.dat");
-    printMatrix(matrixA, 10, 10, NULL);
+        if (y < 1 || w < 1 || v < 1)
+        {
+            fprintf(stderr, "numero de linhas das matrizes incorreto\n");
+        }
+        else
+        {
+            char *filename1 = (char *)malloc(100 * sizeof(char));
+            char *filename2 = (char *)malloc(100 * sizeof(char));
+            char *filename3 = (char *)malloc(100 * sizeof(char));
+            char *filenameOut = (char *)malloc(100 * sizeof(char));
+
+            strcpy(filename1, argv[4]);
+            strcpy(filename2, argv[5]);
+            strcpy(filename3, argv[6]);
+            strcpy(filenameOut, argv[7]);
+
+            double *matrix1 = readMatrixDouble(y, w, filename1);
+            double *matrix2 = readMatrixDouble(w, v, filename2);
+            double *matrix3 = readMatrixDouble(v, 1, filename3);
+
+            /**
+             * A y x w 
+             * B w x v 
+             * C v x 1
+             * D y x 1
+             */
+
+            double *matrix4 = multMatrix(y, w, matrix1, w, v, matrix2);
+            double *matrixRes = multMatrix(y, v, matrix4, v, 1, matrix3);
+
+            printMatrixDouble(matrixRes, y, 1, filenameOut);
+        }
+    }
+    
 
     return 0;
 }
--- OpenMP/src/multMatrix.h
+++ OpenMP/src/multMatrix.h
@@ -1,6 +1,59 @@
-#ifndef MULTMATRIX_H
-#define MULTMATRIX_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
 
-int *multMatrix(int lin1, int col1, int *matrix1, int lin2, int col2, int *matrix2);
+// Matrix 2x3 multplication Matrix 3x2 = Matrix 3x3
+// Matrix num_linhas num_colunas
 
-#endif
+/**
+ * Funcao para multiplicar matrizes alocadas em uma única etapa
+ * 
+ * @param lin1 número de linhas da primeira matriz
+ * @param col1 número de colunas de primeira matriz
+ * @param matrix1 endereço da primeira matriz
+ * @param lin2 número de linhas da segunda matriz
+ * @param col2 número de colunas da segunda matriz
+ * @param matrix2 endereço da segunda matriz
+ * 
+ * @return Endereço para a matriz multiplicação das mmatrizes matrix1 e matrix2
+ * 
+ */ 
+double *multMatrix(int lin1, int col1, double *matrix1, int lin2, int col2, double *matrix2)
+{
+    /* Verificando se é possível multiplicar as 2 matrizes */
+    if (col1 != lin2)
+    {
+        printf("O numero de colunas da matriz 1 e diferente do numero de linhas da matriz 2\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        // Variáveis para a matriz matrix3
+        int lin3 = lin1;
+        int col3 = col2;
+        int number_el = lin2;
+        double *matrix3 = (double *)malloc(lin3 * col3 * sizeof(double));
+
+        int begin = time(NULL);
+
+        #pragma omp parallel for num_threads(4)
+        for (int i = 0; i < lin1; i++)
+        {
+            for (int j = 0; j < col2; j++)
+            {
+                matrix3[col3*i + j] = 0;
+                for (int k = 0; k < number_el; k++)
+                {
+                    matrix3[col3*i + j] += (matrix1[col1*i + k] * matrix2[col2*k + j]);
+                }
+            }
+        }
+
+        int end = time(NULL);
+        printf("%d\n", end - begin);
+
+        return matrix3;
+    }
+}
+
