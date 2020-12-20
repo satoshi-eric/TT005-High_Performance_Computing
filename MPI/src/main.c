df4874899c5593b6417e57e3ceff06684c85d99a
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2020-12-20 13:39:00 -0300

    implementando diretivas do MPI

---
+++ MPI/src/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openacc.h>
+#include <time.h>
+#include "MatrixIO.h"
+#include "multMatrix.h"
+
+
+int main (int argc, char *argv[])
+{
+    // Tramento de erro de argumentos fornecidos pelo terminal
+    if (argc < 8)
+    {
+        printf("Uso:\n");
+        printf("%s <numero de linhas da matriz A> <numero de linhas da matriz B> <numero de linhas da matriz C> <arquivo A> <arquivo B> <arquivo C> <arquivo D>\n", argv[0]);
+    }
+    else
+    {
+        // argv: ./main y w v arqA.dat arqB.dat arqC.dat arqD.dat 
+        int y = atoi(argv[1]);
+        int w = atoi(argv[2]);
+        int v = atoi(argv[3]);
+
+        // Tratamento de erro para valores de linhas menores ou iguais a 0
+        if (y < 1 || w < 1 || v < 1)
+        {
+            fprintf(stderr, "numero de linhas das matrizes não pode ser menor ou igual a zero\n");
+        }
+        else
+        {        
+            int numThreads = 4;    
+            // Lendo matrizes
+            float *matrix1 = readMatrixFloat(y, w, argv[4]);
+            float *matrix2 = readMatrixFloat(w, v, argv[5]);
+            float *matrix3 = readMatrixFloat(v, 1, argv[6]);
+            
+            clock_t begin = clock(); // Calculando tempo de execução da multiplicação
+            
+            float *matrixRes = multMatrix(y, v, multMatrix(y, w, matrix1, w, v, matrix2), v, 1, matrix3); // Multiplicação de matrizes
+
+            clock_t end = clock(); // Calculando tempo de execução da multiplicação
+
+            double time = ((double) (end - begin)/ (CLOCKS_PER_SEC)); // Tempo de execução da multiplicação
+            
+            printf("Time to multiply the matrices %.8lf s\n", time);
+
+            printf("Reduction sum: %.2f\n", reductionSum(y, 1, matrixRes));
+
+            printMatrixFloat(matrixRes, y, 1, argv[7]);
+        }
+    }
+
+
+    return 0;
+}
--- MPI/src/multMatrix.c
+++ MPI/src/multMatrix.c
@@ -4,6 +4,7 @@
 #include "mpi.h"
 
 #define posicao(I, J, COLUNAS) (I*COLUNAS + J)
+#define TAMANHO 4
 
 
 /**
@@ -27,38 +28,57 @@ float *multMatrix ( int lin1, int col1, float *matrix1, int lin2, int col2, floa
 	}
 	else
 	{
+		int numtasks, rank, sendcount, recvcount, source;
+
 		// Variáveis para a matriz 3: resultado
 		int lin3 = lin1;
 		int col3 = col2;
 		float *matrix3 = (float *) malloc(lin3 * col3 * sizeof(float));
+		float recvbuf[TAMANHO];
 
 		// Contadores
 		int i, j, k;
 
-		for ( i=0; i<lin1; i++ )
-		{
-			for ( j=0; j<col2; j++)
-			{
-				
-				// Multiplicação de matrizes
-				matrix3[posicao(i, j, col3)] = 0; 
+		MPI_Init(&argc, &argv);
+		MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-			}
-		}
+		if(numtasks == TAMANHO){
+			source = 1;
+			sendcount = TAMANHO;
+			recvcount = TAMANHO;
 
-		for (i=0; i<lin1; i++)
-		{
-			for (j=0; j<col2; j++)
+			MPI_Scatter(sendbuf,sendcount,MPI_FLOAT,recvbuf,recvcount,
+            MPI_FLOAT,source,MPI_COMM_WORLD);
+			
+			for ( i=0; i<lin1; i++ )
 			{
-				for (k=0; k<col1; k++)
+				for ( j=0; j<col2; j++)
 				{
-					matrix3[posicao(i, j, col3)] += matrix1[posicao(i, k, col1)] * matrix2[posicao(k, j, col2)];
+					
+					// Multiplicação de matrizes
+					matrix3[posicao(i, j, col3)] = 0; 
+
 				}
 			}
-		}
+
+			for (i=0; i<lin1; i++)
+			{
+				for (j=0; j<col2; j++)
+				{
+					for (k=0; k<col1; k++)
+					{
+						matrix3[posicao(i, j, col3)] += matrix1[posicao(i, k, col1)] * matrix2[posicao(k, j, col2)];
+					}
+				}
+			}
 
 
-		return matrix3;
+			return matrix3;
+		}else
+			printf("O programa requer %d processadores.\n",TAMANHO);
+		
+		MPI_Finalize();
 	}	
 }
 
